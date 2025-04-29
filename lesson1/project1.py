6ad59cef5cace008aa926874573b58c3dbfca79a
krasi <krasimir.xristov@gmail.com>
2025-04-30 00:31:00 +0200

    Add basic input/output game with name prompt and play confirmation

--- lesson1/project1.py
+++ lesson1/project1.py
@@ -2,8 +2,11 @@ name = input("Enter your name: ")
 print("Hello " +  name + " Welcome to my game!")
 
 should_we_play = input("Should we play? (yes/no) ").lower()
+# should_we_play = input("Should we play? (yes/no) ").upper()
 
 if should_we_play == "yes":
     print("Let's play!")
+# elif should_we_play == "YES":
+#         print("WELCOME TO THE GAME!")
 else:
     print("No problem!")
