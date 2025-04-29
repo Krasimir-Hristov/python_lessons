098b0f2ddfc548b7baeb7d91c4494676250df9f5
krasi <krasimir.xristov@gmail.com>
2025-04-30 00:28:27 +0200

    Elif example

--- lesson1/project1.py
+++ lesson1/project1.py
@@ -3,10 +3,9 @@ print("Hello " +  name + " Welcome to my game!")
 
 should_we_play = input("Should we play? (yes/no) ")
 
-play = should_we_play == "yes"
-
-
-if play:
+if should_we_play == "yes":
     print("Let's play!")
+elif should_we_play == "YES":
+    print("LETS PLAY!")
 else:
     print("No problem!")
