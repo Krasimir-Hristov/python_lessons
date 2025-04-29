db8965d328c38e8c1b1cb852f97c118ea4322fee
krasi <krasimir.xristov@gmail.com>
2025-04-30 00:19:10 +0200

    Add initial game setup with user input and welcome message

--- lesson1/project1.py
+++ lesson1/project1.py
@@ -1,2 +1,12 @@
 name = input("Enter your name: ")
-print("Hello" +  name + "Welcome to my game!")
+print("Hello " +  name + " Welcome to my game!")
+
+should_we_play = input("Should we play? (yes/no) ")
+
+play = should_we_play == "yes"
+
+
+if play:
+    print("Let's play!")
+else:
+    print("No problem!")
