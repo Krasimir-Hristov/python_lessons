6e535019134e0ab13f63fe9898eec5a4c6c2b421
krasi <krasimir.xristov@gmail.com>
2025-04-30 00:32:37 +0200

    Add initial game script with basic user input and welcome message

--- lesson1/project1.py
+++ lesson1/project1.py
@@ -4,9 +4,9 @@ print("Hello " +  name + " Welcome to my game!")
 should_we_play = input("Should we play? (yes/no) ").lower()
 # should_we_play = input("Should we play? (yes/no) ").upper()
 
-should_we_play == "y" or should_we_play == "yes"
 
-if should_we_play == "yes":
+
+if should_we_play == "y" or should_we_play == "yes":
     print("Let's play!")
 # elif should_we_play == "YES":
 #         print("WELCOME TO THE GAME!")
