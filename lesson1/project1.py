f8a3d897a4f84661317f2dea780cd450007197eb
krasi <krasimir.xristov@gmail.com>
2025-04-30 00:44:51 +0200

    Add text adventure game with branching choices and multiple endings

--- lesson1/project1.py
+++ lesson1/project1.py
@@ -11,21 +11,39 @@ if should_we_play == "y" or should_we_play == "yes":
     direction = input("Which direction do you want to go? (left/right) ").lower()
 
     if direction == "left":
+
         print("You found a treasure!")
+
         take = input("Do you want to take it? (yes/no) ").lower()
+    
         if take == "y" or take == "yes":
+
             print("You die from poison")
         else:
+
             print("You see how many animal dies near the treasure.")
     elif direction == "right":
+
         print("You found a monster!")
+
         fight = input("Do you want to fight it? (yes/no) ").lower()
+
         if fight == "y" or fight == "yes":
+
             print("You die from the monster.")
         else:
+
             print("You run away.")
-# elif should_we_play == "YES":
-#         print("WELCOME TO THE GAME!")
+
+            options = input("You a river do you want to try to cross it? (yes/no) ").lower()
+
+            if options == "y" or options == "yes":
+
+                print("You die from the river.")
+            else:
+
+                print("You die from the monster.")
+
 else:
     print("No problem!")
 
