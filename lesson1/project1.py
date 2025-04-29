7db50e199d316c90caf7b82fd00fdc7536ac0875
krasi <krasimir.xristov@gmail.com>
2025-04-30 00:38:42 +0200

    Add simple text adventure game with user input and branching paths

--- lesson1/project1.py
+++ lesson1/project1.py
@@ -5,10 +5,28 @@ should_we_play = input("Should we play? (yes/no) ").lower()
 # should_we_play = input("Should we play? (yes/no) ").upper()
 
 
-
 if should_we_play == "y" or should_we_play == "yes":
     print("Let's play!")
+
+    direction = input("Which direction do you want to go? (left/right) ").lower()
+
+    if direction == "left":
+        print("You found a treasure!")
+        take = input("Do you want to take it? (yes/no) ").lower()
+        if take == "y" or take == "yes":
+            print("You die from poison")
+        else:
+            print("You see how many animal dies near the treasure.")
+    elif direction == "right":
+        print("You found a monster!")
+        fight = input("Do you want to fight it? (yes/no) ").lower()
+        if fight == "y" or fight == "yes":
+            print("You die from the monster.")
+        else:
+            print("You run away.")
 # elif should_we_play == "YES":
 #         print("WELCOME TO THE GAME!")
 else:
     print("No problem!")
+
+
