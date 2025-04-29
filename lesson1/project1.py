1f30d7e6df054131c463253a80a520bc214e29fa
krasi <krasimir.xristov@gmail.com>
2025-04-30 00:29:28 +0200

    .lower() and .upper() example

--- lesson1/project1.py
+++ lesson1/project1.py
@@ -1,11 +1,9 @@
 name = input("Enter your name: ")
 print("Hello " +  name + " Welcome to my game!")
 
-should_we_play = input("Should we play? (yes/no) ")
+should_we_play = input("Should we play? (yes/no) ").lower()
 
 if should_we_play == "yes":
     print("Let's play!")
-elif should_we_play == "YES":
-    print("LETS PLAY!")
 else:
     print("No problem!")
